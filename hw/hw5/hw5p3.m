6d8d1dbda4c4563a9da0da8ccf582904c3192142
Andrew Pound <aepound@gmail.com>
2014-11-04 23:11:20 -0700

    Got the matlab code working for prob 3.

--- hw/hw5/hw5p3.m
+++ hw/hw5/hw5p3.m
@@ -1,4 +1,4 @@
-function out= hw5p3(A,V,M)
+function [out V]= hw5p3(A,v,M)
 
 n = length(A);
 
@@ -14,20 +14,39 @@ for kiter = 2:M+1
     for iter = 2:n+1
         if kiter >= A(iter-1)
             p1 = P(iter-1,kiter);
+            v1 = V(iter-1,kiter);
             if kiter - A(iter-1) >=1
                 p2 = P(iter-1,kiter-A(iter-1));
+                v2 = V(iter-1,kiter-A(iter-1));
             else
                 p2 = 0;
+                v2 = 0;
             end
             P(iter,kiter) = max(p1,p2);
+            %if P(iter,kiter)
+            if (p1 && p2)
+                if v1 >= (v2 + v(iter-1))   
+                    V(iter,kiter) = V(iter-1,kiter);
+                else
+                    V(iter,kiter) = V(iter-1,kiter-A(iter-1)) + v(iter-1);
+                end
+            elseif p2
+                V(iter,kiter) = V(iter-1,kiter-A(iter-1)) + v(iter-1);
+            else
+                V(iter,kiter) = V(iter-1,kiter);
+            end
+                
         else
             P(iter,kiter) = P(iter-1,kiter);
+            V(iter,kiter) = V(iter-1,kiter);
         end
     end
     
 end
 
 out = [[0 A]' P];
+V = [ [0 0  0:M];
+      [[0 A]' [ 0 v]' V]];
 end
 
 
